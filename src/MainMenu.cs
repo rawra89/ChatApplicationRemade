f46f3349ba7349fc760e6e66f6b0f0af003f5d1d
dead_thing <mbruh4338@gmail.com>
2023-07-01 18:48:38 -0400

    Organization

---
+++ src/MainMenu.cs
@@ -0,0 +1,14 @@
+namespace ChatApplicationRemade;
+public partial class MainMenu : Form
+{
+	public MainMenu()
+	{
+		InitializeComponent();
+	}
+
+	private void MainMenu_Load(object sender, EventArgs e)
+	{
+		// On load, ask for name.
+		// 
+	}
+}
